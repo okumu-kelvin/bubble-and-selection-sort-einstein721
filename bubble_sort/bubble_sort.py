93a560c6eed22299d1a2873d9d1069e4577bd45b
einstien721 <ogambaeinstien39@gmail.com>
2025-06-20 23:27:22 +0300

    changed some code

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False  # Optimization: track if any swaps happened
+        for j in range(0, n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                # Swap if elements are in wrong order
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break  # Stop early if already sorted
+
