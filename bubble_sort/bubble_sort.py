27d8f7db90d44ff06ea2c4eab866008ccf98b0d9
einstein721 <einstein.ombati@strathmore.edu>
2025-06-20 22:33:19 +0300

    deleted one file

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,15 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False  # Flag to check if a swap happened in this pass
+
+        # Traverse the unsorted part of the array
+        for j in range(0, n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                # Swap adjacent elements if they are in the wrong order
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        # If no swaps occurred, the list is already sorted
+        if not swapped:
+            break
