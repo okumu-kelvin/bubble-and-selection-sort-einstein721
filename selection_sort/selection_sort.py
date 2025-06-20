2ea27d0dcf949c72eae9c3138c5b3dbf4b6bee68
einstein721 <einstein.ombati@strathmore.edu>
2025-06-20 22:36:45 +0300

    new code

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,12 @@
+def find_min_index(arr, start):
+    min_idx = start
+    for i in range(start + 1, len(arr)):
+        if arr[i] < arr[min_idx]:
+            min_idx = i
+    return min_idx
+
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    for i in range(len(arr)):
+        min_index = find_min_index(arr, i)
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
